cc290d6f6debe04a46f4dd4d09a9944ea419ea9d
Guilherme <guilherme.guic@gmail.com>
2017-10-05 07:54:18 -0300

    Inclusão dos componentes do card, footer, headbar e submenu.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeadbarComponent } from './components/shared/headbar/headbar.component';
+import { SubMenuComponent } from './components/shared/sub-menu/sub-menu.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { CardQuarterComponent } from './components/shared/card-quarter/card-quarter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadbarComponent,
+    SubMenuComponent,
+    FooterComponent,
+    CardQuarterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/shared/card-quarter/card-quarter.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-quarter',
+  templateUrl: './card-quarter.component.html'
+})
+export class CardQuarterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
