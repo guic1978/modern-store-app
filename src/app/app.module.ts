4b1771d8ee5639c4568928f8f490b2536261a29b
Guilherme <guilherme.guic@gmail.com>
2017-10-07 17:19:13 -0300

    Construcao dos components sub-menu, productlist, login, signup e o inicio do cart.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+//Shared
 import { HeadbarComponent } from './components/shared/headbar/headbar.component';
 import { SubMenuComponent } from './components/shared/sub-menu/sub-menu.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { CardQuarterComponent } from './components/shared/card-quarter/card-quarter.component';
+
+//Components
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+//Pages
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +24,12 @@ import { CardQuarterComponent } from './components/shared/card-quarter/card-quar
     HeadbarComponent,
     SubMenuComponent,
     FooterComponent,
-    CardQuarterComponent
+    CardQuarterComponent,
+    ProductListComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule
