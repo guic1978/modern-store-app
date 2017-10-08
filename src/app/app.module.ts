a17cae1fb7765154aa3eebc39a71fd3e2bb526e9
Guilherme <guilherme.guic@gmail.com>
2017-10-07 21:43:51 -0300

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+// Rotas
+import { Routing, RoutingProviders } from './app.Routing';
+
+// Root
 import { AppComponent } from './app.component';
 
-//Shared
+// Shared
 import { HeadbarComponent } from './components/shared/headbar/headbar.component';
 import { SubMenuComponent } from './components/shared/sub-menu/sub-menu.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { CardQuarterComponent } from './components/shared/card-quarter/card-quarter.component';
 
-//Components
+// Components
 import { ProductListComponent } from './components/product-list/product-list.component';
 
-//Pages
+// Pages
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
@@ -32,7 +37,10 @@ import { CartPageComponent } from './pages/cart-page/cart-page.component';
     CartPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Routing,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
